6312c049cec009bb9bd9184c7825c5579796384c
Brad Daily <bdaily@rsglab.com>
2021-05-30 20:04:15 -0400

    fix content type

--- endpoints.ts
+++ endpoints.ts
@@ -1,6 +1,17 @@
 export function createJsonResponse<T>(
   response: T,
-  init: ResponseInit
+  init: ResponseInit = {}
 ): Response {
+  if (init.headers) {
+    if (!(init.headers instanceof Headers)) {
+      init.headers = new Headers(init.headers);
+    }
+    init.headers.set("Content-Type", "application/json");
+  } else {
+    init.headers = new Headers({
+      "Content-Type": "application/json",
+    });
+  }
+
   return new Response(JSON.stringify(response), init);
 }
